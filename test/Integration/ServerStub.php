928af7ef382dcbd44c38a350a6969133946b8aad
Nigel Greenway <github@futurepixels.co.uk>
2017-05-17 19:34:35 +0100

    [a] add tests utilitising Docker
    This adds a dependancy on Docker being installed

---
+++ test/Integration/ServerStub.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$slim = new \Slim\App();
+
+// Some slim configuration
+$slim->get('/', function( \Psr\Http\Message\RequestInterface $req, \Psr\Http\Message\ResponseInterface $res) {
+    return $res
+        ->withHeader('Content-Type', 'text/html')
+        ->getBody()
+        ->write('<h1>Welcome to ReactiveSlim</h1>');
+});
+
+(new \ReactiveSlim\Server($slim))
+    ->withHost('0.0.0.0')
+    ->withPort(1351)
+    ->run();
